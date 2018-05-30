96deffb92695bc2ab6ff08550ee66989f0dc82ae
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-05-30 19:49:25 +0100

    Updated Iris flower classification sample to use recently added new features of the library. Also dividing data into training and test sets.

--- examples/fc_iris/fc_iris.cpp
+++ examples/fc_iris/fc_iris.cpp
@@ -1,11 +1,28 @@
-// fc_iris.cpp : Defines the entry point for the console application.
-//
+/*
+    ANNT - Artificial Neural Networks C++ library
+
+    Iris flower classification with Fully Connected ANN
+
+    Copyright (C) 2018, cvsandbox, cvsandbox@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdint.h>
-#include <iterator>
 #include <vector>
 #include <map>
 
@@ -16,12 +33,12 @@ using namespace ANNT;
 using namespace ANNT::Neuro;
 using namespace ANNT::Neuro::Training;
 
-// Name of the Iris data file, which is publicly  available using the link below
+// Name of the Iris data file, which is publicly available using the link below
 // http://archive.ics.uci.edu/ml/datasets/Iris
 static const char* IRIS_DATA_FILE = "data/iris.data";
 
 // Helper function to load Iris data set
-bool LoadData( vector<vector_t>& attributes, vector<uint16_t>& classes )
+bool LoadData( vector<vector_t>& attributes, vector<size_t>& labels )
 {
     bool  ret  = false;
     FILE* file = fopen( IRIS_DATA_FILE, "r" );
@@ -29,9 +46,9 @@ bool LoadData( vector<vector_t>& attributes, vector<uint16_t>& classes )
     if ( file )
     {
         char     buff[256];
-        uint16_t classCounter = 0;
+        size_t   labelsCounter = 0;
 
-        map<string, uint16_t> classMap;
+        map<string, size_t> labelsMap;
 
         while ( fgets( buff, 256, file ) != nullptr )
         {
@@ -51,24 +68,24 @@ bool LoadData( vector<vector_t>& attributes, vector<uint16_t>& classes )
                 {
                     classNamePtr++;
 
-                    uint16_t classId = classCounter;
-                    auto     classIt = classMap.find( classNamePtr );
+                    size_t labelId = labelsCounter;
+                    auto   labelIt = labelsMap.find( classNamePtr );
 
-                    if ( classIt != classMap.end( ) )
+                    if ( labelIt != labelsMap.end( ) )
                     {
-                        classId = classIt->second;
+                        labelId = labelIt->second;
                     }
                     else
                     {
-                        classMap.insert( pair<string, uint16_t>( classNamePtr, classCounter ) );
-                        classCounter++;
+                        labelsMap.insert( pair<string, size_t>( classNamePtr, labelsCounter ) );
+                        labelsCounter++;
                     }
 
                     // Iris data set has only 3 classes, so ignore anything else
-                    if ( classId <= 2 )
+                    if ( labelId <= 2 )
                     {
                         attributes.push_back( vector_t( { attr1, attr2, attr3, attr4 } ) );
-                        classes.push_back( classId );
+                        labels.push_back( labelId );
                     }
                 }
             }
@@ -81,45 +98,31 @@ bool LoadData( vector<vector_t>& attributes, vector<uint16_t>& classes )
     return ret;
 }
 
-vector<vector_t> OneHotEncoding( const vector<uint16_t>& classes, uint16_t classesCount )
+// Helper function to extract test samples (20%) out of Iris data
+template <typename T> vector<T> ExtractTestSamples( vector<T>& allSamples )
 {
-    vector<vector_t> encodedClasses( classes.size( ) );
+    vector<T> testSamples( 30 );
 
-    for ( size_t i = 0; i < classes.size( ); i++ )
-    {
-        encodedClasses[i] = vector_t( classesCount );
-        encodedClasses[i][classes[i]] = 1;
-    }
+    std::move( allSamples.begin( ) +  40, allSamples.begin( ) +  50, testSamples.begin( ) );
+    std::move( allSamples.begin( ) +  90, allSamples.begin( ) + 100, testSamples.begin( ) + 10 );
+    std::move( allSamples.begin( ) + 140, allSamples.begin( ) + 150, testSamples.begin( ) + 20 );
 
-    return encodedClasses;
+    allSamples.erase( allSamples.begin( ) + 140, allSamples.begin( ) + 150 );
+    allSamples.erase( allSamples.begin( ) +  90, allSamples.begin( ) + 100 );
+    allSamples.erase( allSamples.begin( ) +  40, allSamples.begin( ) +  50 );
+
+    return testSamples;
 }
 
-uint16_t MaxIndex( const vector_t& vec )
-{
-    uint16_t maxIndex = 0;
-    auto     maxValue = vec[0];
-
-    for ( size_t i = 1, n = vec.size( ); i < n; i++ )
-    {
-        if ( vec[i] > maxValue )
-        {
-            maxValue = vec[i];
-            maxIndex = static_cast<uint16_t>( i );
-        }
-    }
-
-    return maxIndex;
-}
-
+// Example application's entry point
 int main( int /* argc */, char** /* argv */ )
 {
     printf( "Iris classification example with Fully Connected ANN \n\n" );
 
     vector<vector_t> attributes;
-    vector<uint16_t> classes;
-    vector<vector_t> encodedClasses;
+    vector<size_t>   labels;
 
-    if ( !LoadData( attributes, classes ) )
+    if ( !LoadData( attributes, labels ) )
     {
         printf( "Failed loading Iris database \n\n" );
         return -1;
@@ -127,7 +130,22 @@ int main( int /* argc */, char** /* argv */ )
 
     printf( "Loaded %d data samples \n\n", attributes.size( ) );
 
-    encodedClasses = OneHotEncoding( classes, 3 );
+    // make sure we have expected number of samples
+    if ( attributes.size( ) != 150 )
+    {
+        printf( "The data set is expected to provide 150 samples \n\n" );
+        return -2;
+    }
+
+    // split the data set into two: training (120 samples) and test (30 samples)
+    vector<vector_t> testAttributes = ExtractTestSamples( attributes );
+    vector<size_t>   testLabels     = ExtractTestSamples( labels );
+
+    printf( "Using %d samples for training and %d samples for test \n\n", attributes.size( ), testAttributes.size( ) );
+
+    // perform one hot encoding of train/test classes
+    vector<vector_t> encodedLabels     = XDataEncodingTools::OneHotEncoding( labels, 3 );
+    vector<vector_t> encodedTestLabels = XDataEncodingTools::OneHotEncoding( testLabels, 3 );
 
     // prepare a 3 layer ANN
     shared_ptr<XNeuralNetwork> net = make_shared<XNeuralNetwork>( );
@@ -138,49 +156,38 @@ int main( int /* argc */, char** /* argv */ )
     net->AddLayer( make_shared<XTanhActivation>( ) );
     net->AddLayer( make_shared<XFullyConnectedLayer>( 10, 3 ) );
     net->AddLayer( make_shared<XSigmoidActivation>( ) );
-    //net->AddLayer( make_shared<XSoftMaxActivation>( ) );
   
-    // create training context with Nesterov optimizer and MSE cost function
+    // create training context with Nesterov optimizer and Binary Cross Entropy cost function
     XNetworkTraining netCtx( net,
-                             //make_shared<XAdamOptimizer>( 0.01f ),
-                             make_shared<XAdamOptimizer>( 0.0075f ),
-                             //make_shared<XNesterovMomentumOptimizer>( 0.001f ),
+                             make_shared<XNesterovMomentumOptimizer>( 0.001f ),
                              make_shared<XBinaryCrossEntropyCost>( ) );
-                    
-    printf( "Cost of each sample: \n" );
-    for ( size_t i = 0; i < 150 * 20; i++ )
+
+    // check classification error on the training data with random model
+    ANNT::float_t cost = 0;
+    size_t        correct = 0;
+
+    correct = netCtx.TestClassification( attributes, labels, encodedLabels, &cost );
+
+    printf( "Before training: accuracy = %0.2f%% (%u/%u), cost = %0.4f \n", static_cast<float>( correct ) / attributes.size( ) * 100,
+            correct, attributes.size( ), static_cast<float>( cost ) );
+
+    // train the neural network
+    for ( size_t i = 0; i < 32; i++ )
     {
-        size_t sample = rand( ) % attributes.size( );
-        auto   cost   = netCtx.TrainSample( attributes[sample], encodedClasses[sample] );
+        netCtx.TrainEpoch( attributes, encodedLabels, 5, true );
 
-        printf( "%.04f ", static_cast<float>( cost ) );
+        correct = netCtx.TestClassification( attributes, labels, encodedLabels, &cost );
 
-        if ( ( i % 8 ) == 7 )
-            printf( "\n" );
+        printf( "Epoch %3u : accuracy = %0.2f%% (%u/%u), cost = %0.4f \n", i + 1, static_cast<float>( correct ) / attributes.size( ) * 100,
+                correct, attributes.size( ), static_cast<float>( cost ) );
     }
     printf( "\n" );
 
-    float    cost = 0.0f;
-    uint16_t correct = 0;
-    vector_t output;
+    // check the trained ANN on the test data
+    correct = netCtx.TestClassification( testAttributes, testLabels, encodedTestLabels, &cost );
 
-    for ( size_t i = 0; i < attributes.size( ); i++ )
-    {
-        cost += netCtx.TestSample( attributes[i], encodedClasses[i], output );
-
-        uint16_t maxIndex = MaxIndex( output );
-
-        if ( maxIndex == classes[i] )
-        {
-            correct++;
-        }
-    }
-
-    cost /= attributes.size( );
-
-    printf( "Correct classifications: %u \n", correct );
-    printf( "Average cost: %f \n", cost );
+    printf( "Final test: accuracy = %0.2f%% (%u/%u), cost = %0.4f \n", static_cast<float>( correct ) / testAttributes.size( ) * 100,
+            correct, testAttributes.size( ), static_cast<float>( cost ) );
 
     return 0;
 }
-
