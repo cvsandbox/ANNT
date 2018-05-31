9e8f248098e6910d569125ee9ff4078f902e7d5e
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-05-31 21:53:44 +0100

    Added tool functions for parsing MNIST handwritten digits database.

---
+++ examples/tools/MNISTParser.cpp
@@ -0,0 +1,159 @@
+/*
+    ANNT - Artificial Neural Networks C++ library
+
+    MNIST handwritten digits data set parser
+
+    Copyright (C) 2018, cvsandbox, cvsandbox@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include "MNISTParser.hpp"
+
+using namespace std;
+
+#define MNIST_LABELS_FILE_MAGIC    (0x00000801)
+#define MNIST_IMAGES_FILE_MAGIC    (0x00000803)
+#define LABELS_READING_BUFFER_SIZE (1000)
+
+namespace ANNT {
+
+static uint32_t ReverseEndian32( uint32_t v )
+{
+    return ( v >> 24 ) | ( ( v & 0xFF0000 ) >> 8 ) | ( ( v & 0xFF00 ) << 8 ) | ( ( v & 0xFF ) << 24 );
+}
+
+// Loads labels from the specified MNIST labels' file
+bool MNISTParser::LoadLabels( const std::string& fileName, std::vector<size_t>& labels )
+{
+    FILE* file = fopen( fileName.c_str( ), "rb" );
+    bool  ret  = false;
+
+    if ( file != nullptr )
+    {
+        uint32_t magic = 0, labelsCount = 0;
+
+        fread( &magic, 4, 1, file );
+        fread( &labelsCount, 4, 1, file );
+
+        magic       = ReverseEndian32( magic );
+        labelsCount = ReverseEndian32( labelsCount );
+
+        if ( magic == MNIST_LABELS_FILE_MAGIC )
+        {
+            uint8_t buffer[LABELS_READING_BUFFER_SIZE];
+            size_t  toLoad = labelsCount;
+            size_t  loaded = 0;
+            size_t  read   = 0;
+
+            labels = vector<size_t>( toLoad );
+
+            while ( loaded != toLoad )
+            {
+                size_t nextRead = ( toLoad - loaded );
+
+                if ( nextRead > LABELS_READING_BUFFER_SIZE )
+                {
+                    nextRead = LABELS_READING_BUFFER_SIZE;
+                }
+
+                read = fread( buffer, 1, nextRead, file );
+
+                if ( read == nextRead )
+                {
+                    for ( size_t i = 0; i < read; i++ )
+                    {
+                        labels[loaded + i] = static_cast<size_t>( buffer[i] );
+                    }
+
+                    loaded += read;
+                }
+            }
+
+            ret = true;
+        }
+
+        fclose( file );
+    }
+
+    return ret;
+}
+
+// Loads images from the specified MNIST images' file
+bool MNISTParser::LoadImages( const string& fileName, vector<vector_t>& images,
+                              float_t scaleMin, float_t scaleMax, size_t xPad, size_t yPad )
+{
+    FILE* file = fopen( fileName.c_str( ), "rb" );
+    bool  ret  = false;
+
+    if ( file != nullptr )
+    {
+        uint32_t magic = 0, imageCount = 0, width = 0, height = 0;
+
+        fread( &magic, 4, 1, file );
+        fread( &imageCount, 4, 1, file );
+        fread( &height, 4, 1, file );
+        fread( &width, 4, 1, file );
+
+        magic      = ReverseEndian32( magic );
+        imageCount = ReverseEndian32( imageCount );
+        width      = ReverseEndian32( width );
+        height     = ReverseEndian32( height );
+
+        if ( magic == MNIST_IMAGES_FILE_MAGIC )
+        {
+            size_t   imageSize    = width * height;
+            uint8_t* buffer       = new uint8_t[imageSize];
+            size_t   toLoad       = imageCount;
+            size_t   read         = 0;
+
+            uint32_t paddedWidth  = width  + xPad * 2;
+            uint32_t paddedHeight = height + yPad * 2;
+            uint32_t paddedSize   = paddedWidth * paddedHeight;
+
+            for ( size_t i = 0; i < toLoad; i++ )
+            {
+                if ( ( read = fread( buffer, 1, imageSize, file ) ) == imageSize )
+                {
+                    vector_t image( paddedSize, scaleMin );
+
+                    for ( uint32_t y = 0, i = 0; y < height; y++ )
+                    {
+                        for ( uint32_t x = 0; x < width; x++, i++ )
+                        {
+                            image[( y + yPad ) * paddedWidth + xPad + x] = ( static_cast<float_t>( buffer[i] ) / 255 ) *
+                                                                           ( scaleMax - scaleMin ) + scaleMin;
+                        }
+                    }
+
+                    images.push_back( image );
+                }
+            }
+
+            delete [] buffer;
+
+            ret = true;
+        }
+
+        fclose( file );
+    }
+
+    return ret;
+}
+
+} // namespace ANNT
---
+++ examples/tools/MNISTParser.hpp
@@ -0,0 +1,51 @@
+/*
+    ANNT - Artificial Neural Networks C++ library
+
+    MNIST handwritten digits data set parser
+
+    Copyright (C) 2018, cvsandbox, cvsandbox@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#pragma once
+#ifndef ANNT_MNIST_PARSER_HPP
+#define ANNT_MNIST_PARSER_HPP
+
+#include "Types/Types.hpp"
+
+namespace ANNT {
+
+// Helper functions to load images and labels from MNIST database files
+// http://yann.lecun.com/exdb/mnist/
+//
+class MNISTParser
+{
+private:
+    MNISTParser( );
+
+public:
+
+    // Loads labels from the specified MNIST labels' file
+    static bool LoadLabels( const std::string& fileName, std::vector<size_t>& labels );
+
+    // Loads images from the specified MNIST images' file
+    static bool LoadImages( const std::string& fileName, std::vector<vector_t>& images,
+                            float_t scaleMin, float_t scaleMax, size_t xPad, size_t yPad );
+};
+
+} // namespace ANNT
+
+#endif // ANNT_MNIST_PARSER_HPP
