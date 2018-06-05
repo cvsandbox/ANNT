1fb35a5b0e86615907c33d09b9773622cbc8af2d
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-06-05 22:45:43 +0100

    Added makefiles for fc_mnist and cnn_mnist examples.

---
+++ examples/cnn_mnit/make/src.mk
@@ -0,0 +1,15 @@
+# cnn_mnist example source files
+
+# search path for source files
+VPATH = ../../ \
+        ../../../tools
+
+# source files
+SRC = cnn_mnist.cpp \
+      MNISTParser.cpp
+
+# additional include folders
+INCLUDES += -I../../../tools
+
+OBJ = $(SRC:.cpp=.o)
+
---
+++ examples/fc_mnist/make/src.mk
@@ -0,0 +1,15 @@
+# fc_mnist example source files
+
+# search path for source files
+VPATH = ../../ \
+        ../../../tools
+
+# source files
+SRC = fc_mnist.cpp \
+      MNISTParser.cpp
+
+# additional include folders
+INCLUDES += -I../../../tools
+
+OBJ = $(SRC:.cpp=.o)
+
