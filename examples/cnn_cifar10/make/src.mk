120466b2764eab4a9c6a9bd9d4e492e8da95e1db
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-06-30 07:47:06 +0100

    Added GCC makefile for cnn_cifar10 example.

---
+++ examples/cnn_cifar10/make/src.mk
@@ -0,0 +1,15 @@
+# cnn_mnist example source files
+
+# search path for source files
+VPATH = ../../ \
+        ../../../tools
+
+# source files
+SRC = cnn_cifar10.cpp \
+      CIFARParser.cpp
+
+# additional include folders
+INCLUDES += -I../../../tools
+
+OBJ = $(SRC:.cpp=.o)
+
