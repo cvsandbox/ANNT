c2a13f627a3479442697a2606f9389b82cfde672
Andrew Kirillov <andrew.kirillov@gmail.com>
2018-08-08 14:45:14 +0100

    Added make files for the new recent examples. Updated some other makefiles to copy data files from common storage.

---
+++ examples/fc_time_series/make/src.mk
@@ -0,0 +1,7 @@
+# fc_time_series example source files
+
+# search path for source files
+VPATH = ../../
+
+# source files
+SRC = fc_time_series.cpp
---
+++ examples/rnn_time_series/make/src.mk
@@ -0,0 +1,7 @@
+# rnn_time_series example source files
+
+# search path for source files
+VPATH = ../../
+
+# source files
+SRC = rnn_time_series.cpp
